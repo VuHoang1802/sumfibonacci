cffcdf49034b8ccf4bd0949083941c74d1ca26ea
Hoàng Vũ <dev.vuhoang1802@gmail.com>
2024-06-07 19:23:48 +0700

    fÄix: năm

--- src/App.jsx
+++ src/App.jsx
@@ -204,7 +204,7 @@ const App = () => {
         </>
       ) : (
         <button className="text-white px-4 py-2 bg-slate-700 rounded-md" onClick={joinChat}>
-          Tham gia
+          Start
         </button>
       )}
     </div>
